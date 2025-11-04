06154e325bec5b9f3c58b2020193e554c6821384
LohanFirstOne <lqueron@edenschool.fr>
2025-11-04 09:38:10 +0100

    commit

---
+++ exo01.js
@@ -0,0 +1,6 @@
+function greetings(firstname) {
+	return `bienvenu ${firstname}`;
+}
+
+console.log(greetings("")); // Résultat attendu: "Bienvenue, !"
+console.log(greetings("Ben")); // Résultat attendu: "Bienvenue, Ben !"
---
+++ exo09.js
@@ -0,0 +1,51 @@
+let
+function encodePassword(password, encodeFunction) {
+	return encodeFunction(password);
+}
+
+function encodeRot8(password) {
+	let result = "";
+
+	for (let i = 0; i < password.length; i++) {
+		const char = password[i];
+		const code = char.charCodeAt(0);
+
+		if (code >= 97 && code <= 122) {
+			const newCode = ((code - 97 + 8) % 26) + 97;
+			result += String.fromCharCode(newCode);
+		} else if (code >= 65 && code <= 90) {
+			const newCode = ((code - 65 + 8) % 26) + 65;
+			result += String.fromCharCode(newCode);
+		} else {
+			result += char;
+		}
+	}
+
+	return result;
+}
+
+function encodeRot16(password) {
+	let result = "";
+
+	for (let i = 0; i < password.length; i++) {
+		const char = password[i];
+		const code = char.charCodeAt(0);
+
+		if (code >= 97 && code <= 122) {
+			const newCode = ((code - 97 + 16) % 26) + 97;
+			result += String.fromCharCode(newCode);
+		} else if (code >= 65 && code <= 90) {
+			const newCode = ((code - 65 + 16) % 26) + 65;
+			result += String.fromCharCode(newCode);
+		} else {
+			result += char;
+		}
+	}
+
+	return result;
+}
+
+console.log(encodePassword(abc, encodeRot8));
+console.log(encodePassword(abc, encodeRot16));
+console.log(encodePassword(abc, encodeRot8));
+console.log(encodePassword(abc, encodeRot16));
